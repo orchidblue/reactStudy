9489d5f69ec16adc9f19b05d145f0d6c4cc0ca54
User-PC\User <cyorchid@gmail.com>
2018-07-03 15:49:15 +0900

    리액트 작업#1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const goals = [
+  "1. React 개발에 필요한 환경을 구축한다.",
+  "2. 새로운 자바스크립트 문법을 익힌다.",
+  "3. 개발 편의를 위한 VSCODE IDE를 익힌다.",
+  "3. 개발 편의를 위한 VSCODE IDE를 익힌다."
+];
+
+const LectureGoalList = props => {
+  console.log(props.items);
+
+  return (
+    <div>
+      <div>{props.title}</div>
+      <ul>{props.items.map((item, idx) => <li key={idx}>{item}</li>)}</ul>
+    </div>
+  );
+};
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     x:1
+  //   };
+
+  //   //this.handleClick = this.handleClick.bind(this);
+  // }
+
+  state = {
+    x: 1
+  };
+
+  handleClick = () => {
+    console.log(this);
+    console.log(this.state.x);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img onClick={this.handleClick} src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <LectureGoalList items={goals} title={"강의목표"} />
       </div>
     );
   }
