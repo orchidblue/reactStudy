6908fda1355df64b73e6334c8aa7a085edf4e58e
User-PC\User <cyorchid@gmail.com>
2018-07-04 10:50:11 +0900

    LectureGoalList 컴포넌트 구체화

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <LectureGoalList items={goals} title={"강의목표"} />
+        <LectureGoalList items={goals} title={'강의목표'} />
       </div>
     );
   }
---
+++ src/component/LectureGoalList.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+
+const styles = {
+  textAlign: 'left',
+  paddingLeft: '20px',
+  border: '1px solid red',
+  margin: '5px',
+};
+
+const ulStyles = {
+  listStyle: 'none',
+};
+
+const chkStyle = {
+  marginRight: '10px',
+};
+
+const chkBox = idx => {
+  function chkClickEvent(e) {
+    //e.preventDefault();
+    console.log('The link was clicked.  index: ' + e);
+  }
+
+  //return <input type="checkbox" onClick={chkClickEvent} style={chkStyle} />;
+  //react를 사용하여 인자값 전달 방법
+  //https://stackoverflow.com/questions/29810914/react-js-onclick-cant-pass-value-to-method
+  return (
+    <input
+      type="checkbox"
+      //onClick = {() => chkClickEvent(idx)}
+      onClick={function() {
+        chkClickEvent(idx);
+      }}
+      style={chkStyle}
+    />
+  );
+};
+
+const LectureGoalList = ({ items, title }) => {
+  console.log(items);
+
+  return (
+    <div style={styles}>
+      <div>{title}</div>
+      <ul>
+        {items.map((item, idx) => {
+          return (
+            <li key={idx} style={ulStyles}>
+              <label>
+                {chkBox(idx)}
+                {item}
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default LectureGoalList;
