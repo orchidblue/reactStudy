f05ad7a75628652b89432b1eff506aa373bd2a4e
User-PC\User <cyorchid@gmail.com>
2018-07-04 15:08:11 +0900

    react component 구조도

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const Test1 = () => <div>이것도 컴포넌트다!</div>;
+const Test2 = () => {
+  return <div>이것도 컴포넌트다!</div>;
+};
+
+console.log(Test1);
+console.log(Test2);
+console.log(<Test2 />);
+console.log(Test2());
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
